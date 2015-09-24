afb762442e1a9d1e76e447da8533f960a31961bc
Paritosh Pandey <paritoshp73@gmail.com>
2015-09-24 16:38:31 +0530

    Update ossn_com.php

--- ossn_com.php
+++ ossn_com.php
@@ -1,1 +1,18 @@
+<?php
+/**
+ *    OpenSource-SocialNetwork
+ *
+ * @package   
+ * @author    
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES 
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+
+function ajax-search_init() {
+	
+	
+}
+
+ossn_register_callback('ossn', 'init', 'ajax-search_init');
 
