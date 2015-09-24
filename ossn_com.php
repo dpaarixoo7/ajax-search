19a8215ee38b5b6d0d0cb60a57c40e134dfd89f9
Paritosh Pandey <paritoshp73@gmail.com>
2015-09-24 16:32:54 +0530

    Update ossn_com.php

--- ossn_com.php
+++ ossn_com.php
@@ -1,1 +1,17 @@
+<?php
+/**
+ *    OpenSource-SocialNetwork
+ *
+ * @package   http://inhax.net
+ * @author    
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES 
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
 
+function ajax_search_init() {
+	
+	
+}
+
+ossn_register_callback('ossn', 'init', 'ajax_search_init');
